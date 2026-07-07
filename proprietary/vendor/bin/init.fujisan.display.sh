1b3d4db3880098336c9d7217d3c4de15823c316c
Anya1014CN <han.cf@foxmail.com>
2026-07-07 19:02:00 +0800

    fujisan: seed docked dual-screen defaults

--- proprietary/vendor/bin/init.fujisan.display.sh
+++ proprietary/vendor/bin/init.fujisan.display.sh
@@ -1,8 +1,46 @@
 #!/vendor/bin/sh
 
-[ -e /sys/class/leds/lcd-backlight/brightness ] && echo 200 > /sys/class/leds/lcd-backlight/brightness
-[ -e /sys/class/leds/lcd-backlight-2/brightness ] && echo 0 > /sys/class/leds/lcd-backlight-2/brightness
-[ -e /sys/class/graphics/fb0/blank ] && echo 0 > /sys/class/graphics/fb0/blank
-[ -e /sys/class/graphics/fb1/blank ] && echo 4 > /sys/class/graphics/fb1/blank
-[ -e /sys/class/graphics/fb2/blank ] && echo 4 > /sys/class/graphics/fb2/blank
-[ -e /proc/touchscreen/integrate_device_mode ] && echo 0 > /proc/touchscreen/integrate_device_mode
+getprop_int() {
+    local value
+    value="$(getprop "$1")"
+    if [ -n "$value" ]; then
+        echo "$value"
+        return
+    fi
+    echo "$2"
+}
+
+write_if_exists() {
+    if [ -e "$1" ]; then
+        echo "$2" > "$1"
+    fi
+}
+
+display_mode="$(getprop_int persist.vendor.fujisan.display_mode 4)"
+single_display="$(getprop_int persist.vendor.fujisan.single_display_id 0)"
+secondary_backlight="$(getprop_int persist.vendor.fujisan.secondary_backlight 60)"
+hall_status="$(getprop_int persist.sys.zte.hallStatus 1)"
+boot_completed="$(getprop sys.boot_completed)"
+
+write_if_exists /sys/class/leds/lcd-backlight/brightness 200
+write_if_exists /sys/class/graphics/fb0/blank 0
+write_if_exists /proc/touchscreen/integrate_device_mode 0
+
+if [ "$boot_completed" = "1" ]; then
+    settings put system display_mode "$display_mode"
+    settings put system hallC_display_mode "$display_mode"
+    settings put system displayid_single_mode "$single_display"
+    settings put system user_choose_displayid_single_mode "$single_display"
+    settings put system dual_screen_dock_mode_screen_orientation_mode 0
+    settings put secure switch_dispaly_screen_gesture_enabled 1
+fi
+
+if [ "$hall_status" = "3" ] && [ "$display_mode" = "4" ]; then
+    write_if_exists /sys/class/leds/lcd-backlight-2/brightness "$secondary_backlight"
+    write_if_exists /sys/class/graphics/fb1/blank 0
+    write_if_exists /sys/class/graphics/fb2/blank 0
+else
+    write_if_exists /sys/class/leds/lcd-backlight-2/brightness 0
+    write_if_exists /sys/class/graphics/fb1/blank 4
+    write_if_exists /sys/class/graphics/fb2/blank 4
+fi
