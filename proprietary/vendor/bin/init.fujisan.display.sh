77fecefa11ca6366905d7ab07a003932010abfa8
Anya1014CN <han.cf@foxmail.com>
2026-07-09 21:12:34 +0800

    fujisan: sync secondary panel brightness

--- proprietary/vendor/bin/init.fujisan.display.sh
+++ proprietary/vendor/bin/init.fujisan.display.sh
@@ -34,11 +34,84 @@ get_setting_int() {
     esac
 }
 
+get_current_main_backlight() {
+    local value
+
+    if [ -r /sys/class/leds/lcd-backlight/brightness ]; then
+        value="$(cat /sys/class/leds/lcd-backlight/brightness 2>/dev/null | tr -d '\r')"
+        case "$value" in
+            ''|*[!0-9]*)
+                ;;
+            *)
+                echo "$value"
+                return
+                ;;
+        esac
+    fi
+
+    echo "$secondary_backlight"
+}
+
 write_if_exists() {
     if [ -e "$1" ]; then
         echo "$2" > "$1"
     fi
 }
+
+sync_brightness_loop() {
+    local display_mode
+    local single_display
+    local secondary_backlight
+    local hall_status
+    local force_dual
+    local secondary_enabled
+    local target_backlight
+    local last_backlight
+
+    last_backlight=""
+    while true; do
+        display_mode="$(getprop_int persist.vendor.fujisan.display_mode 1)"
+        single_display="$(getprop_int persist.vendor.fujisan.single_display_id 0)"
+        secondary_backlight="$(getprop_int persist.vendor.fujisan.secondary_backlight 60)"
+        hall_status="$(getprop_int persist.sys.zte.hallStatus 1)"
+        force_dual="$(getprop_int persist.vendor.fujisan.force_dual_screen 0)"
+        secondary_enabled=0
+
+        case "$display_mode" in
+            2|4|8)
+                if [ "$hall_status" = "3" ] || [ "$force_dual" = "1" ]; then
+                    secondary_enabled=1
+                fi
+                ;;
+            1)
+                if [ "$single_display" = "1" ]; then
+                    secondary_enabled=1
+                fi
+                ;;
+        esac
+
+        if [ "$secondary_enabled" = "1" ]; then
+            if [ "$display_mode" = "1" ] && [ "$single_display" = "1" ]; then
+                target_backlight="$secondary_backlight"
+            else
+                target_backlight="$(get_current_main_backlight)"
+            fi
+        else
+            target_backlight=0
+        fi
+
+        if [ "$target_backlight" != "$last_backlight" ]; then
+            write_if_exists /sys/class/leds/lcd-backlight-2/brightness "$target_backlight"
+            last_backlight="$target_backlight"
+        fi
+
+        sleep 1
+    done
+}
+
+if [ "${1:-}" = "--watch-brightness" ]; then
+    sync_brightness_loop
+fi
 
 detect_secondary_fb() {
     local fb_cnt
@@ -71,6 +144,7 @@ hall_status="$(getprop_int persist.sys.zte.hallStatus 1)"
 boot_completed="$(getprop sys.boot_completed)"
 force_dual="$(getprop_int persist.vendor.fujisan.force_dual_screen 0)"
 settings_bin="/system/bin/settings"
+synced_backlight="$(get_current_main_backlight)"
 open_mode_fallback=2
 secondary_state=0
 secondary_enabled=0
@@ -126,17 +200,15 @@ fi
 if [ "$display_mode" = "1" ] && [ "$single_display" = "1" ]; then
     write_if_exists /sys/class/leds/lcd-backlight/brightness 0
     write_if_exists /sys/class/graphics/fb0/blank 4
-    write_if_exists /sys/class/leds/lcd-backlight-2/brightness "$secondary_backlight"
+    write_if_exists /sys/class/leds/lcd-backlight-2/brightness "$synced_backlight"
     write_if_exists "/sys/class/graphics/fb$secondary_fb/blank" 0
     write_if_exists "/sys/class/graphics/fb$other_fb/blank" 4
 elif [ "$secondary_enabled" = "1" ]; then
-    write_if_exists /sys/class/leds/lcd-backlight/brightness 200
     write_if_exists /sys/class/graphics/fb0/blank 0
-    write_if_exists /sys/class/leds/lcd-backlight-2/brightness "$secondary_backlight"
+    write_if_exists /sys/class/leds/lcd-backlight-2/brightness "$synced_backlight"
     write_if_exists "/sys/class/graphics/fb$secondary_fb/blank" 0
     write_if_exists "/sys/class/graphics/fb$other_fb/blank" 4
 else
-    write_if_exists /sys/class/leds/lcd-backlight/brightness 200
     write_if_exists /sys/class/graphics/fb0/blank 0
     write_if_exists /sys/class/leds/lcd-backlight-2/brightness 0
     write_if_exists "/sys/class/graphics/fb$secondary_fb/blank" 4
