124ba17a6c81832357fade9eb02090af9e6bef21
Anya1014CN <han.cf@foxmail.com>
2026-07-20 15:35:05 +0800

    fujisan: package fingerprint service as override module

--- Android.mk
+++ Android.mk
@@ -23,4 +23,18 @@ LOCAL_MULTILIB := both
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := fujisan_fingerprint_service
+LOCAL_MODULE_OWNER := zte
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := proprietary/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.biometrics.fingerprint@2.1-service
+LOCAL_INIT_RC := proprietary/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc
+LOCAL_MULTILIB := 64
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_OVERRIDES_MODULES := android.hardware.biometrics.fingerprint@2.1-service
+include $(BUILD_PREBUILT)
+
 endif
