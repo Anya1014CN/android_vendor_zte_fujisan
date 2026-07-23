0d2f77dcf111dad94a53e02b34d88b2f2b2d5ef4
Anya1014CN <han.cf@foxmail.com>
2026-07-23 12:54:56 +0800

    fujisan: force sys.usb.configfs=1 in stock USB boot path
    Legacy android_usb composition is not writable on the 4.4
    gadget stack. Keep stock gadget/ffs setup and select configfs.

--- proprietary/vendor/bin/init.vendor.usb.sh
+++ proprietary/vendor/bin/init.vendor.usb.sh
@@ -1,4 +1,4 @@
 #!/vendor/bin/sh
-# Fujisan bring-up: stock USB composition helpers disabled.
-# Configfs adb is owned by init.fujisan.usb.rc.
+# Fujisan bring-up: stock composition helper disabled.
+# USB is owned by configfs (init.qcom.usb.rc + fujisan.usb.rc / AOSP init.usb.configfs.rc).
 exit 0
