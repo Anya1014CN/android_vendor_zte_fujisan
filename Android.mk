b86522d361f252152cf5fccfab94671424e4f4c3
Anya1014CN <han.cf@foxmail.com>
2026-07-31 14:20:07 +0800

    fujisan: add C2D backends for hardware media codecs

--- Android.mk
+++ Android.mk
@@ -20,6 +20,35 @@ LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libgpustats.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MULTILIB := both
+# libgsl and libCB are supplied by the device vendor image but are not
+# registered as build modules. Keep the prebuilt's existing vendor ABI.
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libc2d30-a5xx
+LOCAL_MODULE_OWNER := zte
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libc2d30-a5xx.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libc2d30-a5xx.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := both
+# GPU C2D backend paired with the existing device libC2D2.so.
+LOCAL_CHECK_ELF_FILES := false
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libc2d30_bltlib
+LOCAL_MODULE_OWNER := zte
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libc2d30_bltlib.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libc2d30_bltlib.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MULTILIB := both
+# GPU C2D backend paired with the existing device libC2D2.so.
+LOCAL_CHECK_ELF_FILES := false
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
